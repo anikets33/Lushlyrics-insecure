edde28090c9a8ba60b2649e8bc702a357fff0ff6
Aniket Singh <aniketsingh4771@gmail.com>
2024-03-03 14:09:20 +0530

    Changed few css properties and added firstname and lastname in views

--- lushlyrics-webapp-django-main/lushlyrics-webapp-django-main/main/views.py
+++ lushlyrics-webapp-django-main/lushlyrics-webapp-django-main/main/views.py
@@ -74,6 +74,8 @@ def add_playlist(request):
 
 def user_signup(request):
    if request.method == 'POST':
+      first_name = request.POST.get('first_name')
+      last_name = request.POST.get('last_name')
       username = request.POST.get('username')
       email = request.POST.get('email')
       password = request.POST.get('password')
@@ -86,7 +88,7 @@ def user_signup(request):
         messages.info(request, 'Email is already registered. Please login to continue.')
         return redirect('/signup/')
 
-      user = User.objects.create(username=username, email=email)
+      user = User.objects.create(first_name=first_name,last_name=last_name, username=username, email=email)
       user.set_password(password)
 
       user.save()
